2a30ffb73268008cfa7ba32e18faeb4594873568
VIRTUALVASANTH <109015929+VIRTUALVASANTH@users.noreply.github.com>
2025-02-21 17:04:35 +0530

    Update NMT.py

--- NMT.py
+++ NMT.py
@@ -1,61 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-pip install sacremoses
-
-
-# In[1]:
-
-
-import torch
-from transformers import MarianMTModel, MarianTokenizer
-import gradio as gr
-
-# Define source and target languages
-source_lang = "en"
-target_lang = "fr"
-model_name = f"Helsinki-NLP/opus-mt-{source_lang}-{target_lang}"
-
-# Load tokenizer and model
-tokenizer = MarianTokenizer.from_pretrained(model_name)
-model = MarianMTModel.from_pretrained(model_name)
-
-# Function to translate text
-def translate_text(text):
-    if not text:
-        return "Please enter some text."
-    
-    # Tokenize input text
-    input_tokens = tokenizer(text, return_tensors="pt", padding=True, truncation=True)
-    
-    # Generate translation
-    with torch.no_grad():
-        translated_tokens = model.generate(**input_tokens)
-
-    # Decode output
-    translated_text = tokenizer.decode(translated_tokens[0], skip_special_tokens=True)
-    return translated_text
-
-# Define Gradio interface
-interface = gr.Interface(
-    fn=translate_text,
-    inputs="text",
-    outputs="text",
-    title="Real-Time Language Translator",
-    description="Enter text in English and get the translated text in French."
-)
-
-# Launch the Gradio app
-if _name_ == "_main_":
-    interface.launch()
-
-
-# In[3]:
-
-
 import torch
 from transformers import MarianMTModel, MarianTokenizer
 import gradio as gr
@@ -97,10 +39,3 @@ interface = gr.Interface(
 # Launch the Gradio app
 if __name__ == "__main__":
     interface.launch()
-
-
-# In[ ]:
-
-
-
-
